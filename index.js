a6e398c9906d947b21d5bf0da4f2b2d243666473
Victor Emmanuel <victorpassos2012@gmail.com>
2022-04-01 15:52:35 -0300

    Criando as rotas, mas nenhuma funionalidade

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const sales = require('./router/salesroute');
+const products = require('./router/productroute');
 require('dotenv').config();
+
+const app = express();
+app.use(bodyParser.json());
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.use('/sales', sales);
+app.use('/products', products);
 
 app.listen(process.env.PORT, () => {
   console.log(`Escutando na porta ${process.env.PORT}`);
