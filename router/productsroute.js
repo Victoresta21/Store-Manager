a3c6f8761b61e66b2f2a0d9530a4891927743fb3
Victor Emmanuel <victorpassos2012@gmail.com>
2022-04-04 20:22:20 -0300

    Requisito 4 feito

--- router/productsroute.js
+++ router/productsroute.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
-const { getAll, getById } = require('../controllers/products');
-const { validateBody } = require('../middlewares/products');
+const { getAll, getById, create } = require('../controllers/products');
+const { validateBody, validateNameAlreadyExists } = require('../middlewares/products');
 
 router.get('/', getAll);
 router.get('/:id', getById);
-router.post('/', validateBody);
+router.post('/', validateBody, validateNameAlreadyExists, create);
 router.put('/:id', validateBody);
 
 module.exports = router;
