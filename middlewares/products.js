4221515eaf71aa07d8f6ebdd2b3e388bc9404885
Victor Emmanuel <victorpassos2012@gmail.com>
2022-04-05 03:06:18 -0300

    Requisito 5 feito

--- middlewares/products.js
+++ middlewares/products.js
@@ -1,5 +1,5 @@
 const Joi = require('joi');
-const { getByName } = require('../services/products');
+const { getByName, getById } = require('../services/products');
 
 const productSchema = Joi.object({
   name: Joi.string().min(5).required(),
@@ -26,7 +26,18 @@ const validateNameAlreadyExists = async (req, res, next) => {
   return next();
 };
 
+const validateProductExists = async (req, res, next) => {
+  const { id } = req.params;
+
+  const product = await getById(id);
+
+  if (!product) return res.status(404).json({ message: 'Product not found' });
+
+  return next();
+};
+
 module.exports = {
   validateBody,
   validateNameAlreadyExists,
+  validateProductExists,
 };
--- router/productsroute.js
+++ router/productsroute.js
@@ -1,10 +1,14 @@
 const router = require('express').Router();
-const { getAll, getById, create } = require('../controllers/products');
-const { validateBody, validateNameAlreadyExists } = require('../middlewares/products');
+const { getAll, getById, create, update } = require('../controllers/products');
+const {
+validateBody,
+validateNameAlreadyExists,
+validateProductExists,
+} = require('../middlewares/products');
 
 router.get('/', getAll);
 router.get('/:id', getById);
 router.post('/', validateBody, validateNameAlreadyExists, create);
-router.put('/:id', validateBody);
+router.put('/:id', validateBody, validateProductExists, update);
 
 module.exports = router;
