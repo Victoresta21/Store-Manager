0e7317d366449698857723ab170217889e7f3283
Victor Emmanuel <victorpassos2012@gmail.com>
2022-04-06 02:04:08 -0300

    Requisito 7 feito

--- router/salesroute.js
+++ router/salesroute.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
-const { getAll, getById } = require('../controllers/sales');
+const { getAll, getById, create } = require('../controllers/sales');
 const { validateBody } = require('../middlewares/sales');
 
 router.get('/', getAll);
 router.get('/:id', getById);
-router.post('/', validateBody);
+router.post('/', validateBody, create);
 router.put('/:id', validateBody);
 
 module.exports = router;
